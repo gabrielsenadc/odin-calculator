1a85cdcf26665e2d3fac7881b5e2139030171968
gabrielsenadc <gabriel.cunha.74@edu.ufes.br>
2024-03-10 19:01:27 -0300

    realizando operacoes sem digitar numeros negativos e decimais

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 const result = document.querySelector(".result");
-const num1 = document.querySelector("#num1");
-const num2 = document.querySelector("#num2");
-const num3 = document.querySelector("#num3");
-const num4 = document.querySelector("#num4");
-const num5 = document.querySelector("#num5");
-const num6 = document.querySelector("#num6");
-const num7 = document.querySelector("#num7");
-const num8 = document.querySelector("#num8");
-const num9 = document.querySelector("#num9");
-const num0 = document.querySelector("#num0");
+const num = [];
 const divide = document.querySelector("#divide");
 const mult = document.querySelector("#multiply");
 const sub = document.querySelector("#subtract");
@@ -19,46 +10,84 @@ const clear = document.querySelector("#clear");
 let print = "";
 let first = 0;
 let second = 0;
-let operation = "";
 let n = 0;
-
-function addNumberToPrint(number){
-    print += number;
-    result.textContent = print;
-}
+let operation = "";
 
 clear.addEventListener("click", () =>{
     result.textContent = "";
     print = "";
 });
 
-num1.addEventListener("click", () => {addNumberToPrint("1")});
+let id = ""
+for(let i = 0; i < 10; i++){
+    id = "#num" + i
+    num[i] = document.querySelector(id);
+    num[i].addEventListener("click", () => {
+        print += i;
+        result.textContent = print;
+    });
+}
 
-num2.addEventListener("click", () => {addNumberToPrint("2")});
-
-num3.addEventListener("click", () => {addNumberToPrint("3")});
-
-num4.addEventListener("click", () => {addNumberToPrint("4")});
-
-num5.addEventListener("click", () => {addNumberToPrint("5")});
-
-num6.addEventListener("click", () => {addNumberToPrint("6")});
-
-num7.addEventListener("click", () => {addNumberToPrint("7")});
-
-num8.addEventListener("click", () => {addNumberToPrint("8")});
-
-num9.addEventListener("click", () => {addNumberToPrint("9")});
-
-num0.addEventListener("click", () => {addNumberToPrint("0")});
-
-add.addEventListener("click", () =>{
-    if(!n) first = Number(print);
-    print += "+";
+function addOperation(op){
+    if(!n) {
+        first = Number(print);
+        n++;
+    } else{
+        Operation();
+        first = Number(print);
+    }
+    print += " " + op + " ";
     result.textContent = print;
-    console.log(first);
-    operation = "add";
+    operation = op;
+}
+
+add.addEventListener("click", () =>{addOperation("+")});
+mult.addEventListener("click", () =>{addOperation("*")});
+divide.addEventListener("click", () =>{addOperation("/")});
+sub.addEventListener("click", () =>{addOperation("-")});
+
+function Operation(){
+    if(operation === "+"){
+        pos = print.indexOf("+");
+        second = Number(print.slice(pos + 1));
+        console.log(second);
+        print = String(first + second);
+        result.textContent = print;
+    }
+
+    if(operation === "-"){
+        pos = print.indexOf("-");
+        second = Number(print.slice(pos + 1));
+        console.log(second);
+        print = String(first - second);
+        result.textContent = print;
+    }
+
+    if(operation === "/"){
+        pos = print.indexOf("/");
+        second = Number(print.slice(pos + 1));
+        console.log(second);
+        print = String(first / second);
+        result.textContent = print;
+    }
+
+    if(operation === "*"){
+        pos = print.indexOf("*");
+        second = Number(print.slice(pos + 1));
+        console.log(second);
+        print = String(first * second);
+        result.textContent = print;
+    }
+}
+
+let pos = 0;
+equal.addEventListener("click", () => {
+    Operation();
+    n--;
 });
+
+
+
 
 
 
