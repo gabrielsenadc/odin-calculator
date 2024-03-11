62d936cafb853bf5d1ebd5899cd098201807d272
gabrielsenadc <gabriel.cunha.74@edu.ufes.br>
2024-03-11 20:28:57 -0300

    adicionando operações com decimais

--- script.js
+++ script.js
@@ -6,16 +6,31 @@ const sub = document.querySelector("#subtract");
 const add = document.querySelector("#add");
 const equal = document.querySelector("#equal");
 const clear = document.querySelector("#clear");
+const dot = document.querySelector("#dot");
 
 let print = "";
-let first = 0;
-let second = 0;
+let firstNum = 0;
+let secondNum = 0;
 let n = 0;
 let operation = "";
+let temDot = 0;
 
 clear.addEventListener("click", () =>{
     result.textContent = "";
     print = "";
+    secondNum = 0;
+    firstNum = 0;
+    operation = "";
+});
+
+dot.addEventListener("click", () => {
+    if(!temDot){
+        if(print == "") print = "0";
+        else if (operation != "" && print.slice(print.indexOf(operation) + 1) == " ") print += "0";
+        print += ".";
+        result.textContent = print;
+        temDot++;
+    }
 });
 
 let id = ""
@@ -30,15 +45,16 @@ for(let i = 0; i < 10; i++){
 
 function addOperation(op){
     if(!n) {
-        first = Number(print);
+        firstNum = Number(print);
         n++;
     } else{
         Operation();
-        first = Number(print);
+        firstNum = Number(print);
     }
     print += " " + op + " ";
     result.textContent = print;
     operation = op;
+    temDot = 0;
 }
 
 add.addEventListener("click", () =>{addOperation("+")});
@@ -49,35 +65,38 @@ sub.addEventListener("click", () =>{addOperation("-")});
 function Operation(){
     if(operation === "+"){
         pos = print.indexOf("+");
-        second = Number(print.slice(pos + 1));
-        console.log(second);
-        print = String(first + second);
-        result.textContent = print;
+        secondNum = Number(print.slice(pos + 1));
+        console.log(secondNum);
+        print = String(firstNum + secondNum);
     }
 
     if(operation === "-"){
         pos = print.indexOf("-");
-        second = Number(print.slice(pos + 1));
-        console.log(second);
-        print = String(first - second);
-        result.textContent = print;
+        secondNum = Number(print.slice(pos + 1));
+        console.log(secondNum);
+        print = String(firstNum - secondNum);
     }
 
     if(operation === "/"){
         pos = print.indexOf("/");
-        second = Number(print.slice(pos + 1));
-        console.log(second);
-        print = String(first / second);
-        result.textContent = print;
+        secondNum = Number(print.slice(pos + 1));
+        console.log(secondNum);
+        print = String(firstNum / secondNum);
     }
 
     if(operation === "*"){
         pos = print.indexOf("*");
-        second = Number(print.slice(pos + 1));
-        console.log(second);
-        print = String(first * second);
-        result.textContent = print;
+        secondNum = Number(print.slice(pos + 1));
+        console.log(secondNum);
+        print = String(firstNum * secondNum);
     }
+    if(print.includes("."))
+        if((print.slice(print.indexOf(".") + 1)).length > 15) print = String(Number(print).toFixed(2));
+    
+    result.textContent = print;
+    secondNum = 0;
+    temDot = 0;
+    operation = "";
 }
 
 let pos = 0;
